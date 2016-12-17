4135b38a9d1ce93f328f29da84b65045318929a0
mdjdrn <mdjdrn1@gmail.com>
2016-12-17 16:50:54 +0100

    Simplified get_pixel and draw_pixel

--- project/SDL_function.cpp
+++ project/SDL_function.cpp
@@ -12,29 +12,9 @@
 Uint32 SDL::get_pixel(SDL_Surface* surface, const int& x, const int& y)
 {
 	int bpp = surface->format->BytesPerPixel;
-	/* Here p is the address to the pixel we want to retrieve */
-	Uint8* p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+	Uint8* p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp; // addres to pixel to get
 
-	switch (bpp)
-	{
-	case 1:
-		return *p;
-
-	case 2:
-		return *(Uint16 *)p;
-
-	case 3:
-		if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
-			return p[0] << 16 | p[1] << 8 | p[2];
-		else
-			return p[0] | p[1] << 8 | p[2] << 16;
-
-	case 4:
-		return *(Uint32 *)p;
-
-	default:
-		return 0; /* shouldn't happen, but avoids warnings */
-	}
+	return *(reinterpret_cast<Uint32 *>(p));
 }
 
 /** \brief Draw pixel into SDL_Surface
@@ -55,35 +35,9 @@ void SDL::draw_pixel(SDL_Surface* surface, const int& x, const int& y, const uin
 	int bpp = surface->format->BytesPerPixel;
 	Uint8* p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp; // ptr to pixel we want to set
 
-	switch (bpp)
-	{
-	case 1:
-		*p = pixel;
-		break;
-
-	case 2:
-		*(Uint16 *)p = pixel;
-		break;
-
-	case 3:
-		if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
-		{
-			p[0] = (pixel >> 16) & 0xff;
-			p[1] = (pixel >> 8) & 0xff;
-			p[2] = pixel & 0xff;
-		}
-		else
-		{
-			p[0] = pixel & 0xff;
-			p[1] = (pixel >> 8) & 0xff;
-			p[2] = (pixel >> 16) & 0xff;
-		}
-		break;
-
-	case 4:
-		*(Uint32 *)p = pixel;
-		break;
-	}
+	p[0] = pixel & 0xff;
+	p[1] = (pixel >> 8) & 0xff;
+	p[2] = (pixel >> 16) & 0xff;
 }
 
 /** \brief Get pixel RGB values from SDL_Surface
