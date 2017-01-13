897fcb6782a043da31e390c38476b1928d73d8b7
marzgra <marzgra@o2.pl>
2017-01-13 19:18:35 +0100

    Update RLE.h

--- RLE.h
+++ RLE.h
@@ -1,13 +1,29 @@
-#pragma once
-
-#ifndef RLE_H_
-#define RLE_H_
-
 #include <string>
-#include <cstdint>
 
-	void Decode(const char *input, const char *output);
-	void Encode(const char *input, const char *output);
+using namespace std;
+
+typedef struct SPixel {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+}PIXEL;
+
+class RLE{
+private:
+	bool compare(SPixel pixel1, SPixel pixel2) {
+		if (pixel1.red == pixel2.red && pixel1.green == pixel2.green && pixel1.blue == pixel2.blue)
+			return true;
+		else return false;
+	};
+	PIXEL next;
+	PIXEL current;
+	unsigned int repetition = 1;
+
+public:
+	RLE();
+	virtual ~RLE();
+	void encode(string &input, string &output);
+	void decode(string &input, string &output);
+};
 
 
-#endif
