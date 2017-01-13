d5721d333c7ae5a18910d554cd21156ac8de5397
marzgra <marzgra@o2.pl>
2017-01-13 19:17:51 +0100

    Update RLE.cpp

--- RLE.cpp
+++ RLE.cpp
@@ -1,61 +1,84 @@
-#include "RLE.h"
-#include <stdio.h>
+#include <string>
 #include <fstream>
+#include <iostream>
+#include <cstdint>
+#include "RLE.h"
 
 using namespace std;
 
-void Encode(const char *input, const char *output) {
-	fstream file;					// original file
-	fstream compressed;				// compresed file
-	char current;
-	char next;
-	int fsize = 0;					// file size
-	int repetitions = 1;
-	
-	file.open(input, ios::in | ios::ate | ios::binary);
-	compressed.open(output, ios::out | ios::trunc | ios::binary);
+RLE::RLE() {} ;
+RLE::~RLE() {} ;
 
-	fsize = file.tellg();
-		
-	for (int i = 0; i < fsize; i++){
-		file.seekg(i, ios::beg);
-		file.read((char*)&current, sizeof(char)); // get current character
-		next = file.peek();
-		
-		if (next != current){
-			compressed.write((char*)&repetitions, sizeof(char));
-			compressed.write((char*)&current, sizeof(char));
-			repetitions = 0;
-		}
-		repetitions++;
+void RLE::encode(string &input, string &output){
+
+	fstream file;
+	fstream compressed;
+
+	file.open(input, ios::in | ios::binary);
+	if (!file.is_open()) {
+		cout << "cannot open file to encode." << endl;
+		getchar();
+		exit(1);
 	}
+
+	compressed.open(output, ios::out | ios::trunc | ios::binary);
+	if (!compressed.is_open()) {
+		cout << "cannot open file to save encoded file." << endl;
+		getchar();
+		exit(1);
+	}
+
+	file.read((char*)&current, sizeof(PIXEL));	// read first two pixeles
+	file.read((char*)&next, sizeof(PIXEL));
+	
+	while (!file.eof()){
+
+		if (!compare(current, next)) {		// if different, write repetitions and pixel to file
+			compressed.write((char*)&repetition, sizeof(repetition));
+			compressed.write((char*)&current, sizeof(PIXEL));
+			repetition = 0;
+		}
+		repetition++;
+		current = next;							
+		file.read((char*)&next, sizeof(PIXEL));	// read next pixel
+
+		if (file.eof()) {			// if end of file, write repetitions and pixel to file
+			compressed.write((char*)&repetition, sizeof(repetition));
+			compressed.write((char*)&current, sizeof(PIXEL));
+		}
+	}
+
 	file.close();
 	compressed.close();
 }
 
-
-void Decode(const char *input, const char *output) {
+void RLE::decode(string &input, string &output) {
 	fstream file;
-	fstream ufile;
-	char current;
-	char next;
-	int repetitions = 0;
-	int fsize = 0;
-	
-	file.open(input, ios::ate | ios::in | ios::binary);
-	ufile.open(output, ios::trunc | ios::out | ios::binary);
-	fsize = file.tellg();	
+	fstream ready;
 
+	file.open(input, ios::in | ios::binary);
+	if (!file.is_open()) {
+		cout << "cannot open file to decode." << endl;
+		getchar();
+		exit(1);
+	}
+	ready.open(output, ios::trunc | ios::out | ios::binary);
+	if (!ready.is_open()) {
+		cout << "cannot open file to save decoded file." << endl;
+		getchar();
+		exit(1);
+	}
 
-	for (int i = 0; i < fsize; i += 2){
-		file.seekg(i, ios::beg);
-		file.read((char*)&repetitions, sizeof(char));
-		file.seekg(i + 1, ios::beg);
-		file.read((char*)&current, sizeof(char));
-		for (int j = 0; j < repetitions; j++)
-			ufile.write((char*)&current, sizeof(char));
+	while (!file.eof()){
+		file.read((char*)&repetition, sizeof(repetition));		// read repetitions
+		file.read((char*)&current, sizeof(PIXEL));			// read pixel
+		if (file.eof()) break;
+		for (int j = 0; j < repetition; j++)				// write pixel 'repetition' times
+			ready.write((char*)&current, sizeof(PIXEL));
 	}
 
 	file.close();
-	ufile.close();
+	ready.close();
 }
+
+
