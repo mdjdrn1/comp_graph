1f1071c93f3636a6e3bdbfc99cc91f24ad4b7961
marzgra <marzgra@o2.pl>
2017-01-22 18:54:44 +0100

    Update RLE.cpp

--- RLE.cpp
+++ RLE.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <fstream>
 #include <iostream>
 #include <cstdint>
@@ -13,6 +12,7 @@ void RLE::encode(string &input, string &output){
 
 	fstream file;
 	fstream compressed;
+	vcopy.clear();
 
 	file.open(input, ios::in | ios::binary);
 	if (!file.is_open()) {
@@ -30,22 +30,53 @@ void RLE::encode(string &input, string &output){
 
 	file.read((char*)&current, sizeof(PIXEL));	// read first two pixeles
 	file.read((char*)&next, sizeof(PIXEL));
-	
-	while (!file.eof()){
 
-		if (!compare(current, next)) {		// if different, write repetitions and pixel to file
-			compressed.write((char*)&repetition, sizeof(repetition));
-			compressed.write((char*)&current, sizeof(PIXEL));
-			repetition = 0;
-		}
-		repetition++;
-		current = next;							
-		file.read((char*)&next, sizeof(PIXEL));	// read next pixel
+	while (!file.eof()) {
 
-		if (file.eof()) {			// if end of file, write repetitions and pixel to file
-			compressed.write((char*)&repetition, sizeof(repetition));
-			compressed.write((char*)&current, sizeof(PIXEL));
+		if (compare(current, next)) {				// identical pixels
+			if (!vcopy.empty()) {					// save vector to file
+				vcopy.pop_back();
+				compressed.write((char*)&vstart, sizeof(vstart));
+				compressed.write((char*)&vsize, sizeof(vsize));
+				for (int i = 0; i < vsize; i++)
+					compressed.write((char*)&vcopy[i], sizeof(PIXEL));
+				vcopy.clear();
+			}
+			if (repetition < 255)					
+				++repetition;
+			else {
+				compressed.write((char*)&repetition, sizeof(repetition));
+				compressed.write((char*)&current, sizeof(PIXEL));
+				repetition = 2;
+			}
 		}
+		else {								
+			if (repetition > 1) {		
+				compressed.write((char*)&repetition, sizeof(repetition));
+				compressed.write((char*)&current, sizeof(PIXEL));
+			}
+			repetition = 1;
+			
+			if (vsize < 255) 
+				vcopy.push_back(next);
+			else {
+				compressed.write((char*)&vstart, sizeof(vstart));
+				compressed.write((char*)&vsize, sizeof(vsize));
+				for (int i = 0; i < vsize; i++)
+					compressed.write((char*)&vcopy[i], sizeof(PIXEL));
+				vcopy.clear();
+				vcopy.push_back(next);
+				
+			}
+		}
+
+		current = next;
+		file.read((char*)&next, sizeof(PIXEL));
+	}
+
+	if (file.eof()) {						// if end of file, write repetitions and pixel to file
+		compressed.write((char*)&repetition, sizeof(repetition));
+		compressed.write((char*)&current, sizeof(PIXEL));
 	}
 
 	file.close();
@@ -70,11 +101,22 @@ void RLE::decode(string &input, string &output) {
 	}
 
 	while (!file.eof()){
-		file.read((char*)&repetition, sizeof(repetition));		// read repetitions
-		file.read((char*)&current, sizeof(PIXEL));			// read pixel
+		file.read((char*)&repetition, sizeof(repetition));	// read repetitions
+		//file.read((char*)&current, sizeof(PIXEL));			// read pixel
 		if (file.eof()) break;
-		for (int j = 0; j < repetition; j++)				// write pixel 'repetition' times
-			ready.write((char*)&current, sizeof(PIXEL));
+		uint8_t count = 0;
+
+		if (repetition == 0) {
+			file.read((char*)&count, sizeof(count));
+			file.read((char*)&current, sizeof(PIXEL));
+			for (unsigned int i = 0; i < count; i++)
+				ready.write((char*)&current, sizeof(PIXEL));
+		}
+		else {
+			file.read((char*)&current, sizeof(PIXEL));
+			for (int j = 0; j < repetition; j++)				// write pixel 'repetition' times
+				ready.write((char*)&current, sizeof(PIXEL));
+		}
 	}
 
 	file.close();
